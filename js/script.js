a30e3d0711ffff8a2941aadcd3081f364448d111
arun <arun@medzdigital.com>
2019-06-04 19:05:36 +0530

    svg list as array

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+
 var imgList = [{
 			"Screen": 'Wall',
 			"Image": 'image-01.svg'
@@ -12,9 +14,10 @@ var imgList = [{
 			"Image": 'image-03.svg'
 		}];
 
+        var svgList = [];
 
 		for (var images = 0; images < imgList.length; images++) {
-			 $('<li><svg x="0px" y="0px" width="1080px" height="1920px" viewBox="0 0 1080 1920" style="enable-background:new 0 0 1080 1920;" xml:space="preserve"><use xlink:href="'+imgList[images].Image+'#Layer_1"></use></svg></li>').appendTo('.bxslider');
+            svgList.push($('<li><svg x="0px" y="0px" width="1080px" height="1920px" viewBox="0 0 1080 1920" style="enable-background:new 0 0 1080 1920;" xml:space="preserve"><use xlink:href="'+imgList[images].Image+'#Layer_1"></use></svg></li>').appendTo('.bxslider'));
 		}
 
 		for (var thumbnail = 0; thumbnail < imgList.length; thumbnail++) {
